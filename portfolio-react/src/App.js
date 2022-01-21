66ed1697fe2eed511c8bb2fe9bfdba3dcae9523b
Tesfa-Worku <hopegorg@gmail.com>
2022-01-21 02:59:17 -0500

    refactor(app): refactor navigation route

--- portfolio-react/src/App.js
+++ portfolio-react/src/App.js
@@ -1,4 +1,5 @@
-
+import { useState } from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Nav from '../src/Components/Nav';
 import Home from "../src/Components/Home";
@@ -10,27 +11,46 @@ import Contact from './Components/Contact';
 
 
 function App() {
+
+  const [currentPage, setCurrentPage] = useState('Profile');
+
   return (
     <div>
-      <Nav />
+      <HashRouter>
+        <Nav
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+        />
 
-      <div className="bg-light">
-        <div className="row g-lg-0 h-100">
-            <div className="col-lg-12">
-                <div className="bg-light text-dark m-3 p-3 border rounded-3 clearfix">
-                  <Home />
-                  <CareerDevelopment />
-                  <Portfolio />
-                  <Education />
-                  <Contact />
-                </div>            
-            </div>
+        <div className="bg-light">
+          <div className="row g-lg-0 h-100">
+              <div className="col-lg-12">
+                  <div className="bg-light text-dark m-3 p-3 border rounded-3 clearfix">
+                    <Routes>
+                      <Route exact activeClassName="active" path='/' element={
+                        <Home />
+                      } />
+                      <Route exact activeClassName="active" path='/career-development' element={
+                        <CareerDevelopment />
+                      } />
+                      <Route exact activeClassName="active" path='/portfolio' element={
+                        <Portfolio />
+                      } />
+                      <Route exact activeClassName="active" path='/education' element={
+                        <Education />
+                      } />
+                      <Route exact activeClassName="active" path='/contact' element={
+                        <Contact />
+                      } />
+                    </Routes>
+                  </div>            
+              </div>
+          </div>
+
         </div>
 
-      </div>
-
-      <Footer />
-
+        <Footer />
+      </HashRouter>
     </div>
   );
 }
