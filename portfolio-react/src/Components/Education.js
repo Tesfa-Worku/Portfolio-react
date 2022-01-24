119feeae436d229c7802a394e2bee73bbc2d1398
Tesfa-Worku <hopegorg@gmail.com>
2022-01-24 15:03:45 -0500

    refactor(education): update list for schools

--- portfolio-react/src/Components/Education.js
+++ portfolio-react/src/Components/Education.js
@@ -4,9 +4,15 @@ const Education = () => {
         <div>
             <h2>Education</h2>
             <ul>
-                <li>Certificate-Bitwise Industries</li>
-                <li>Certificate-Evangadi Tech</li>
-                <li>Bsc</li>                        
+                <li><strong>Workforce training at Bitwise Industries - Certificate</strong> <br />
+July, 2021 - August, 2021 <br />
+Study JavaScript and React.</li>
+                <li><strong>Evangadi Academy - Certificate</strong> <br />
+February, 2021 - June, 2021 <br />
+Full-Stack web development</li>
+                <li><strong>Arba-Minch University - Bsc</strong> <br />
+October, 2008 - July, 2011 <br />
+B.Sc in Mechanical Engineering</li>                       
             </ul>
         </div>
     )
