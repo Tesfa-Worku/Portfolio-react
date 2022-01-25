a77b74be1188e17c9fdcd07cfc7ddbe2d1700a2b
Tesfa-Worku <hopegorg@gmail.com>
2022-01-24 23:31:45 -0500

    chore(portfolio): make link to open in new tab

--- portfolio-react/src/Components/Portfolio.js
+++ portfolio-react/src/Components/Portfolio.js
@@ -7,19 +7,19 @@ const Portfolio = () => {
             </h2>
             <ul className="navbar-nav flex-column border-light border-top w-100">
                 <li className="nav-item border-light border-bottom">
-                    <a className="nav-link" href="https://luceroweb.github.io/haunted-house-game/#/">Haunted House Game</a>
+                    <a className="nav-link" href="https://luceroweb.github.io/haunted-house-game/#/" target="_blank" rel="noreferrer">Haunted House Game</a>
                 </li>
                 <li className="nav-item border-light border-bottom">
-                    <a className="nav-link" href="https://tesfa-worku.github.io/Decidr/">Decider</a>
+                    <a className="nav-link" href="https://tesfa-worku.github.io/Decidr/" target="_blank" rel="noreferrer">Decider</a>
                 </li>
                 <li className="nav-item border-light border-bottom">
-                    <a className="nav-link" href="https://tesfa-worku.github.io/ecard/">ecard</a>
+                    <a className="nav-link" href="https://tesfa-worku.github.io/ecard/" target="_blank" rel="noreferrer">ecard</a>
                 </li>
                 <li className="nav-item border-light border-bottom">
-                    <a className="nav-link" href="https://expo.dev/@tesfa-worku/team-marvel-project">Marvel Space</a>
+                    <a className="nav-link" href="https://expo.dev/@tesfa-worku/team-marvel-project" target="_blank" rel="noreferrer">Marvel Space</a>
                 </li>
                 <li className="nav-item border-light border-bottom">
-                    <a className="nav-link" aria-current="page" href="https://github.com/Tesfa-Worku/todo-list-app">To do App</a>
+                    <a className="nav-link" aria-current="page" href="https://github.com/Tesfa-Worku/todo-list-app" rel="noreferrer" target="_blank">To do App</a>
                 </li>
             </ul>
         </div>
